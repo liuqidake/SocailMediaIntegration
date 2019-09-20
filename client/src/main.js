44b85388cd1557b94802d7fc9f4374606c2ed5b3
liuqidake <qiliuchina@yahoo.com>
2019-09-19 19:25:57 -0600

    change stargety by using Vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
